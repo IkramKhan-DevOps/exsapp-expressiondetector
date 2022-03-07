95af1308b1011ad0aab39d637aa49dff55b3dd3e
IkramKhan-DevOps <ikram.khan0762@gmail.com>
2022-03-07 08:18:34 -0800

    PROJECT >> init

---
+++ README.md
@@ -0,0 +1,47 @@
+<p align="center">
+  <a href="https://exarth.com/"><img src="https://exarth.com/static/exarth/theme/logo-red-1000.svg" height="150"></a>
+</p><hr>
+
+# E Wallet
+## Sent your cash anywhere
+
+Online Wallet application to transfer funds and to pay bills of credit card, with mastercard, visa, american express, google pay and apple pay all payments are supported
+
+## Features
+ - 
+
+## Tech
+
+These technologies are used in projects to work properly:
+
+- [HTML5] - HTML enhanced for web apps!
+- [CSS3] - Awesome Designing
+- [Javascript] - Client Side Scripts
+- [JQuerry] - great UI boilerplate for modern web apps
+- [Python] - World Top Language for Top Developers
+- [Django] - Web Framework for perfectionists
+- [SQlite3] - the streaming build system
+
+## Installation
+
+Application requires [Python > 3.1](https://www.python.org/downloads/release/python-380/) to run, please install Python first
+Install the dependencies and devDependencies and start the server.
+
+```sh
+cd directory
+pip install -r requirements.txt
+py manage.py runserver
+```
+
+#### How to run
+Open terminal and run the command below, make sure all requirements are installed previously
+```sh
+cd directory
+py manage.py runserver
+```
+Follow to link to view your application [http://127.0.0.1:8000/](http://127.0.0.1:8000/)
+## License
+MIT
+
+### Resources
+https://exarth.com/
---
+++ core/settings.py
@@ -0,0 +1,75 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = 'django-insecure-8e#qaf_(x#do=$--t+%jpqfrop!#@5s9e=a_y0v35(0kqdav7i'
+DEBUG = True
+ROOT_URLCONF = 'core.urls'
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'core.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
+STATIC_URL = 'static/'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
